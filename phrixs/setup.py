0e7d3cb3ce0cb8b7783e5a43891a2091d598299c
Lucy <Air@MacBook-Air-Macbook.local>
2019-11-23 18:18:50 +0300

    - setup modified

--- phrixs/setup.py
+++ phrixs/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
      long_description = fh.read()
 print(setuptools.find_packages(),)
 setuptools.setup(
-    name="phrixs", 
+    name="phrixs",
     version="0.0.0",
     author="Andrey Geondzhian",
     # author_email="andrey@example.com",
@@ -13,6 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     # url="https://github.com/pypa/sampleproject",
     packages=[],
-    install_requires=['numpy','pyqtgraph','scipy','tqdm','pathos'],
+    install_requires=['numpy','pyqtgraph','scipy','tqdm','pathos','matplotlib'],
     python_requires='>=3.6',
 )
