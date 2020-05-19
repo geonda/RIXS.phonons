1109d7914ba86abd543847c2585aea5e1357ec51
Andrey Geondzhian <lusigeondzian@MacBook-Air-Lusi.local>
2020-05-20 01:33:26 +0300

    update docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Andrey Geondzhian, Keith Gilmore'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.0.dev3'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
